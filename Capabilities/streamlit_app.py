1bae9218c4926a1608fb5fbd3de7343a44ca0aa6
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-08-29 13:05:03 -0400

    Update streamlit_app.py

--- Capabilities/streamlit_app.py
+++ Capabilities/streamlit_app.py
@@ -1,7 +1,21 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+from chalice import Chalice
+from chalicelib import storage_service
+from chalicelib import transcription_service
+import base64
+import json
+
+#####
+# chalice app configuration
+#####
+
+#####
+# services initialization
+#####
+storage_location = 'aws-bucket-fun'
+storage_service = storage_service.StorageService(storage_location)
+transcription_service = transcription_service.TranscriptionService(storage_service)
+
 
 """
 #  Welcome to Reflect!
