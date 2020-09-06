42ea1a5cacd72150d1703ddf319a25babb7cec6d
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-09-06 19:27:18 -0400

    Update streamlit_app.py

--- Capabilities/streamlit_app.py
+++ Capabilities/streamlit_app.py
@@ -1,21 +1,20 @@
 import streamlit as st
-from chalice import Chalice
-from chalicelib import storage_service
-from chalicelib import transcription_service
-import base64
+from chalicelib import storage_service, transcription_service
+from chalicelib import nlp_viz
+#import base64
 import json
 import os
 from os import path
 from PIL import Image
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-import matplotlib.pyplot as plt
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-import nltk
-nltk.download('punkt')
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import seaborn as sns
+#from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+#import matplotlib.pyplot as plt
+#from nltk.tokenize import word_tokenize
+#from nltk.corpus import stopwords
+#from nltk.stem import PorterStemmer
+#import nltk
+#nltk.download('punkt')
+#from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+#import seaborn as sns
 import pandas as pd
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -44,30 +43,33 @@ if filename:
 
     transcription_text = transcription_service.transcribe_audio(recording_id)
     st.write(transcription_text)
-
-    wordcloud = WordCloud().generate(transcription_text)
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
-    st.pyplot()
-
-    text = word_tokenize(transcription_text)
-    stopWords = set(stopwords.words('english'))
-    text = " ".join([w for w in text if w not in stopWords])
-    #ps = PorterStemmer()
     
-    analyser = SentimentIntensityAnalyzer()
-    score = analyser.polarity_scores(text)
-    st.write(f"{score}")
-           
-    df_sent = pd.DataFrame({k:[v] for k, v in score.items()}).T.reset_index() 
-    df_sent.columns = ["Sentiment", "Frequency"] 
-    plt.figure(figsize=(4,3))
-    sns.set(style="white")
-    ax = sns.barplot(y="Sentiment", x="Frequency",data=df_sent)
-    plt.xlabel('Sentiment')
-    plt.ylabel('Frequency')
-    plt.title('Polarity')
-    st.pyplot()
+    nlp_viz.wordcloud_viz(transcription_text)
+    nlp_viz.sentiment_viz(transcription_text)
+
+#    wordcloud = WordCloud().generate(transcription_text)
+#    plt.imshow(wordcloud, interpolation='bilinear')
+#    plt.axis("off")
+#    st.pyplot()
+#
+#    text = word_tokenize(transcription_text)
+#    stopWords = set(stopwords.words('english'))
+#    text = " ".join([w for w in text if w not in stopWords])
+#    #ps = PorterStemmer()
+#    
+#    analyser = SentimentIntensityAnalyzer()
+#    score = analyser.polarity_scores(text)
+#    st.write(f"{score}")
+#           
+#    df_sent = pd.DataFrame({k:[v] for k, v in score.items()}).T.reset_index() 
+#    df_sent.columns = ["Sentiment", "Frequency"] 
+#    plt.figure(figsize=(4,3))
+#    sns.set(style="white")
+#    ax = sns.barplot(y="Sentiment", x="Frequency",data=df_sent)
+#    plt.xlabel('Sentiment')
+#    plt.ylabel('Frequency')
+#    plt.title('Polarity')
+#    st.pyplot()
 
     
  
@@ -80,30 +82,33 @@ if filename is not None:
 
     transcription_text = transcription_service.transcribe_audio(recording_id)
     st.write(transcription_text)
-
-    wordcloud = WordCloud().generate(transcription_text)
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
-    st.pyplot()
-
-    text = word_tokenize(transcription_text)
-    stopWords = set(stopwords.words('english'))
-    text = " ".join([w for w in text if w not in stopWords])
-    #ps = PorterStemmer()
     
-    analyser = SentimentIntensityAnalyzer()
-    score = analyser.polarity_scores(text)
-    st.write(f"{score}")
-           
-    df_sent = pd.DataFrame({k:[v] for k, v in score.items()}).T.reset_index() 
-    df_sent.columns = ["Sentiment", "Frequency"] 
-    plt.figure(figsize=(4,3))
-    sns.set(style="white")
-    ax = sns.barplot(y="Sentiment", x="Frequency",data=df_sent)
-    plt.xlabel('Sentiment')
-    plt.ylabel('Frequency')
-    plt.title('Polarity')
-    st.pyplot()
+    nlp_viz.wordcloud_viz(transcription_text)
+    nlp_viz.sentiment_viz(transcription_text)
+
+#    wordcloud = WordCloud().generate(transcription_text)
+#    plt.imshow(wordcloud, interpolation='bilinear')
+#    plt.axis("off")
+#    st.pyplot()
+#
+#    text = word_tokenize(transcription_text)
+#    stopWords = set(stopwords.words('english'))
+#    text = " ".join([w for w in text if w not in stopWords])
+#    #ps = PorterStemmer()
+#    
+#    analyser = SentimentIntensityAnalyzer()
+#    score = analyser.polarity_scores(text)
+#    st.write(f"{score}")
+#           
+#    df_sent = pd.DataFrame({k:[v] for k, v in score.items()}).T.reset_index() 
+#    df_sent.columns = ["Sentiment", "Frequency"] 
+#    plt.figure(figsize=(4,3))
+#    sns.set(style="white")
+#    ax = sns.barplot(y="Sentiment", x="Frequency",data=df_sent)
+#    plt.xlabel('Sentiment')
+#    plt.ylabel('Frequency')
+#    plt.title('Polarity')
+#    st.pyplot()
 
     
     
